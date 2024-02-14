8002419750ffdd855afe542158222fb84aba63dd
Kernel-rb <matabsaifeddine@gmail.com>
2024-02-14 15:52:36 +0100

    probleme 2

---
+++ 2 - Merge Two Sorted Lists/solution.js
@@ -0,0 +1,7 @@
+var mergeTwoLists = function(list1, list2) {
+    const arr = list1.concat(list2)
+    
+    console.log(arr.sort())  
+};
+
+res = mergeTwoLists([1,2,4] ,  [1,3,4])
---
+++ 2 - Merge Two Sorted Lists/solution.py
@@ -0,0 +1,6 @@
+def mergeTwolists(list1 , list2):
+    arr = list1 + list2
+    arr.sort()
+    print(arr)
+
+res = mergeTwolists([1,2,4] , [1,3,4])
