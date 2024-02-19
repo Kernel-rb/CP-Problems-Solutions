d4d0cac8ba1c3168ed1f6ff5f1deccdace5d6de2
Kernel-rb <matabsaifeddine@gmail.com>
2024-02-19 14:02:08 +0100

    sum done TODO: JS & RS

---
+++ 7-Two Sum/s.py
@@ -0,0 +1,15 @@
+def twoSum(nums:list[int] , target:int) :
+    arr = []
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == target:
+                arr.append(i)
+                arr.append(j)
+                return arr
+    return arr
+
+        
+            
+print(twoSum([2,7,11,15], 9))
+
+# https://leetcode.com/problems/two-sum/
