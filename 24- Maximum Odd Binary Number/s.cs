e8cbb1a4cb07adcfb7b24657c85fb3fb10b3de81
Kernel-rb <matabsaifeddine@gmail.com>
2024-03-09 12:59:53 +0100

    update cs

--- 24- Maximum Odd Binary Number/s.cs
+++ 24- Maximum Odd Binary Number/s.cs
@@ -1,25 +1,15 @@
-class Solution {
-
-    /**
-     * @param Integer $num1
-     * @param Integer $num2
-     * @return Integer
-     */
-    function countOperations($n1, $n2) 
-    {
-        $count = 0;
-        while($n1 > 0 and $n2 > 0)
-        {
-            if($n1 > $n2)
-            {
-                $n1 -= $n2;
-                $count += 1;
-            }
-            else{
-                $n2 -= $n1;
-                $count += 1;
+public class Solution {
+    public int CountOperations(int n1, int n2) {
+        int ops = 0 ;
+        while( n1>0  && n2> 0 ){
+            if(n1 > n2){
+                n1 -= n2;
+                ops += 1;
+            }else{
+                n2 -= n1;
+                ops += 1;
             }
         }
-        return $count;
+        return ops;
     }
 }
