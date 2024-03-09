39a11fb755a9a43afd9cb9280a4019af3de9e41f
Kernel-rb <matabsaifeddine@gmail.com>
2024-03-09 15:05:02 +0100

    update delete the c file and create an rs file

---
+++ 25- Faulty Keyboard/s.rs
@@ -0,0 +1,13 @@
+pub fn final_string(s: String) -> String {
+  let mut a:String = String::new();
+  for i in s.chars()
+  {
+    if i != 'i'
+    {
+      a.push(i);
+    }else{
+      a = a.chars().rev().collect::<String>();
+    }
+  }
+  return a;
+}
