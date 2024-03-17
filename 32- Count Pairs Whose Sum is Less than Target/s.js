ffdcd4af4624911fc066a09faaaf8cc42038a71d
Kernel-rb <matabsaifeddine@gmail.com>
2024-03-17 14:28:49 +0100

    js & ts solution added

---
+++ 32- Count Pairs Whose Sum is Less than Target/s.js
@@ -0,0 +1,15 @@
+var countPairs = function(nums, target) {
+    let counter = 0;
+    for(let i = 0 ; i < nums.length ;i++)
+    {
+        for(let j = i +1 ; j < nums.length ; j++)
+        {
+            if(nums[i] + nums[j]  < target )
+            {
+                counter++;
+            }
+        }
+    }
+    return counter ;
+    
+};
