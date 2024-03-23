03e1976efe256bc46474d693ede132899b9c2197
Kernel-rb <matabsaifeddine@gmail.com>
2024-03-23 13:21:21 +0100

    add c solution on 29 challenge and !challenge is ....

--- 01-March-2024/29-Minimum Cost of Buying Candies With Discount/s.c
+++ 01-March-2024/29-Minimum Cost of Buying Candies With Discount/s.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Comparator function for sorting in descending order
+
 int compare(const void *a, const void *b) {
     return (*(int*)b - *(int*)a);
 }
 
 int minimumCost(int* cost, int n) {
-    qsort(cost, n, sizeof(int), compare); // Sort the costs in descending order
+    qsort(cost, n, sizeof(int), compare); 
     int total_cost = 0;
     for (int i = 0; i < n; i++) {
-        if (i % 3 != 2) { // Every three candies, one is free
+        if (i % 3 != 2) {
             total_cost += cost[i];
         }
     }
--- 01-March-2024/39-Find the Pivot Integer/s.py
+++ 01-March-2024/39-Find the Pivot Integer/s.py
@@ -1,8 +1,1 @@
-def hammingWeight(n: int) -> int:
-        int_to_bin = bin(n)[2:]
-        count = 0 
-        for i in int_to_bin :
-            if i ==  "1" : 
-                count += 1
-        return count
-
+def countConsistentStrings(allowed: str, words: list[str]) -> int:
