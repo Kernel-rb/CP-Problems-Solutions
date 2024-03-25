ec7c77b4b3a13296688c3b3521755a9daefa3fd0
Kernel-rb <matabsaifeddine@gmail.com>
2024-03-25 16:13:34 +0100

    update Rust solution

--- 01-March-2024/41-Count Items Matching a Rule/s.rs
+++ 01-March-2024/41-Count Items Matching a Rule/s.rs
@@ -1,14 +1,13 @@
-impl Solution {
-    pub fn count_consistent_strings(allowed: String, words: Vec<String>) -> i32 {
-        let mut count = 0;
-        for i in words.iter() {
-            for j in i.chars() {
-                if !allowed.chars().any(|c| c == j) {
-                    count += 1;
-                    break;
-                }
-            }
+pub fn count_matches(items: Vec<Vec<String>>, rule_key: String, rule_value: String) -> i32 {
+    let mut counter:i32 = 0;
+    for i in 0..items.len(){
+        if rule_key == "type" && items[i][0] == rule_value{
+            counter += 1;
+        }else if rule_key == "color" && items[i][1] == rule_value{
+            counter += 1;
+        }else if rule_key == "name" && items[i][2] == rule_value{
+            counter += 1;
         }
-        words.len() as i32 - count
     }
+    return counter
 }
