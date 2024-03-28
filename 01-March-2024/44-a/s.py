31e4e286f66b365dd7b7d6ccf9596d94b571e5c4
Kernel-rb <matabsaifeddine@gmail.com>
2024-03-28 16:35:35 +0100

    new challenge added

--- 01-March-2024/44-a/s.py
+++ 01-March-2024/44-a/s.py
@@ -1,15 +1,5 @@
-def isSameAfterReversals(num: int) -> bool: 
-    str_num = str(num)
-    reverse_str_num = str_num[::-1]
-    for i in reverse_str_num : 
-        if i ==  '0' : 
-            new_reverse_num = reverse_str_num.replace('0','')
-    new_reverse_num = reverse_str_num[::-1]
+def maxProductDifference(nums: list[int]) -> int:
+    nums.sort() 
+    return nums[-1] * nums[-2]  - nums[0] * nums[1]
 
-    if str_num == new_reverse_num : 
-        return True 
-    else : 
-        return False
-
-
-print(isSameAfterReversals(1800))
+print(maxProductDifference([5,6,2,7,4])) ,
