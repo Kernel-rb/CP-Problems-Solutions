d3faede3e0c03375118512f170b4324e48387ada
Kernel-rb <matabsaifeddine@gmail.com>
2024-03-29 21:26:20 +0100

    add c solution

---
+++ 01-March-2024/46-a/s.c
@@ -0,0 +1,13 @@
+int countPairs(int* nums, int numsSize, int k) {
+    int counter =0 ;
+    int  i = 0;
+    int j = 0;
+    for(i = 0; i < numsSize; i++){
+        for(j = i+1; j < numsSize; j++){
+            if ( nums[i] ==  nums[j] && (i * j) > k){
+                counter++;
+            }
+        }
+    }
+    return counter;
+}
