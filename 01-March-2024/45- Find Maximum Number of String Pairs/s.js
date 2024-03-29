b72787c2b245a1878d8cfddd290f6026e915bd89
Kernel-rb <matabsaifeddine@gmail.com>
2024-03-29 14:12:40 +0100

    Js solution added

---
+++ 01-March-2024/45- Find Maximum Number of String Pairs/s.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var maximumNumberOfStringPairs = function(words) {
+    let counter = 0;
+    for(let i = 0 ; i < words.length ; i++){
+        for(let j = i+1 ; j < words.length ;j++){
+            if( words[i] === words[j].split('').reverse().join('')){
+                counter++;
+            }
+        }
+    }
+    return counter;
+};
