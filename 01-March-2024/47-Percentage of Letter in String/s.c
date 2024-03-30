20f517f6758c248c18f8b93c026c38611f74c55b
Kernel-rb <matabsaifeddine@gmail.com>
2024-03-30 13:18:24 +0100

    c solution added

---
+++ 01-March-2024/47-Percentage of Letter in String/s.c
@@ -0,0 +1,11 @@
+int percentageLetter(char* s, char letter) {
+    int counter = 0 ;
+    int i = 0 ;
+    while(s[i] != '\0') {
+        if(s[i] == letter) {
+            counter++ ;
+        }
+        i++ ;
+    }
+    return (counter * 100) / i ;   
+}
