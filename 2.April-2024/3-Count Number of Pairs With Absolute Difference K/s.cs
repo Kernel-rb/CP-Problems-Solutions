ea57f1e762c403224964dd1daaaa09b9322aad30
Kernel-rb <matabsaifeddine@gmail.com>
2024-04-03 21:37:15 +0100

    new challenge  🦊

---
+++ 2.April-2024/3-Count Number of Pairs With Absolute Difference K/s.cs
@@ -0,0 +1,11 @@
+public int CountKDifference(int[] nums, int k) {
+    int count = 0;
+    for (int i = 0; i < nums.Length; i++) {
+        for (int j = i + 1; j < nums.Length; j++) {
+            if (Math.Abs(nums[i] - nums[j]) == k) {
+                count++;
+            }
+        }
+    }
+    return count;        
+}
