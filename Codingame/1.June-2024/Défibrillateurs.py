9795d93f4216ce65f6db8a6a0d3f48978db0cc13
Kernel-rb <matabsaifeddine@gmail.com>
2024-06-16 22:50:14 +0200

    🦊 [+]

--- Codingame/1.June-2024/Défibrillateurs.py
+++ Codingame/1.June-2024/Défibrillateurs.py
@@ -1,33 +1,24 @@
+import sys
 import math
 
-# user  info
-lon = input()
-lon = float(lon.replace(',', '.'))
-lon =  lon * math.pi / 180
-lat = input()
-lat = float(lat.replace("," , "."))
-lat = lat * math.pi / 180
+def convertir_en_rad(r) -> float:
+    return float(r.replace(",", ".")) * math.pi / 180
+
+lon = convertir_en_rad(input())
+lat = convertir_en_rad(input())
 
 n = int(input())
-min_distance = float('inf') 
+min_d = float("inf")
+proche = ""
 for i in range(n):
-    defib = input()
-    arr = defib.split(";")
-    defib_lon = arr[4]
-    defib_lon = float(defib_lon.replace("," , ".")) 
-    defib_lon = defib_lon * math.pi / 180
-    defib_lat = arr[5] 
-    defib_lat = float(defib_lat.replace("," , "."))
-    defib_lat = defib_lat * math.pi / 180
+    defib = input().split(";")
+    defib_lon = convertir_en_rad(defib[-2])
+    defib_lat = convertir_en_rad(defib[-1])
+    z = (defib_lon - lon) * math.cos((defib_lat + lat) / 2)
+    y = defib_lat - lat
+    d = math.sqrt(z**2 + y**2) * 6371
+    if d < min_d:
+        min_d = d
+        proche = defib[1]
 
-    distance = 0
-    x = (defib_lon -lon ) * math.cos((defib_lat + lat) / 2)
-    y = (defib_lat -lat )
-    d =  math.sqrt((x**2) + (y**2)) * 6371
-    if d < min_distance:
-        min_distance = d
-        nearest = arr[1]
-print(nearest)
-
-
-
+print(proche)
